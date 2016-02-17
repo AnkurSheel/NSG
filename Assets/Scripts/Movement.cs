7b5639631c3924cc75cedb475338eba334301289
AnkurSheel <ankur.sheel@gmail.com>
2016-02-17 21:43:01 +1300

    - moving object along dragged path

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,16 +5,20 @@ public class Movement : MonoBehaviour
 {
   public GameObject sphere;
   public float speed = 5;
+  public float pathDepth = -0.2f;
+  Vector3 targetPosition;
+  Vector3 originalPosition;
+  LineRenderer line;
+  int CurrentDragPointIndex;
+  ArrayList dragPoints = new ArrayList();
   bool isDragging = false;
   bool isMouseDown = false;
-  Vector3 targetPosition;
-  ArrayList dragPoints = new ArrayList();
-  LineRenderer line;
 
   // Use this for initialization
   void Start()
   {
     targetPosition = transform.position;
+    CurrentDragPointIndex = 0;
     line = GetComponent<LineRenderer>();
     line.SetWidth(0.1f, 0.1f);
   }
@@ -24,11 +28,12 @@ public class Movement : MonoBehaviour
   {
     if (Input.GetMouseButton(0))
     {
-      if(!isMouseDown)
+      if (!isMouseDown)
       {
         isMouseDown = true;
-        ClearPath();
+        Reset();
       }
+
       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
       RaycastHit hit;
 
@@ -38,7 +43,7 @@ public class Movement : MonoBehaviour
         {
           if (HasPointerMoved())
           {
-            AddDragPoints();
+            AddDragPoint();
             DrawLine();
           }
         }
@@ -50,7 +55,8 @@ public class Movement : MonoBehaviour
           }
           else
           {
-            SetTargetPositionToMoveBackground(hit.point);
+            dragPoints.Clear();
+            AddDragPoint();
           }
         }
       }
@@ -60,15 +66,18 @@ public class Movement : MonoBehaviour
       isMouseDown = false;
       isDragging = false;
     }
+  }
+
+  public void FixedUpdate()
+  {
     MoveObject();
   }
 
-  private void AddDragPoints()
+  private void AddDragPoint()
   {
-    Vector3 mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 4.7f);
-    dragPoints.Add(Camera.main.ScreenToWorldPoint(mPosition));
-    line.SetVertexCount(dragPoints.Count);
-    line.SetPosition(dragPoints.Count - 1, (Vector3)dragPoints[dragPoints.Count - 1]);
+    Vector3 mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z);
+    Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mPosition);
+    dragPoints.Add(worldPoint);
   }
 
   private bool HasPointerMoved()
@@ -81,28 +90,34 @@ public class Movement : MonoBehaviour
     line.SetVertexCount(dragPoints.Count);
     for (int i = 0; i < dragPoints.Count; i++)
     {
-      line.SetPosition(i, (Vector3)dragPoints[i]);
+      Vector3 worldPoint = (Vector3)dragPoints[i];
+      worldPoint.z = pathDepth;
+      line.SetPosition(i, worldPoint);
     }
   }
 
-  private void SetTargetPositionToMoveBackground(Vector3 touchPosition)
-  {
-    targetPosition = transform.position - touchPosition;
-  }
-
-  private void ClearPath()
+  private void Reset()
   {
     dragPoints.Clear();
     line.SetVertexCount(0);
+    CurrentDragPointIndex = 0;
+    originalPosition = transform.position;
   }
 
   private void MoveObject()
   {
-    if (transform.position != targetPosition)
+    if (!isDragging)
     {
-      float step = speed * Time.deltaTime;
-      transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+      if (transform.position != targetPosition)
+      {
+        float step = speed * Time.fixedDeltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+      }
+      else if (CurrentDragPointIndex < dragPoints.Count)
+      {
+        targetPosition = originalPosition - (Vector3)dragPoints[CurrentDragPointIndex];
+        CurrentDragPointIndex++;
+      }
     }
   }
-
 }
