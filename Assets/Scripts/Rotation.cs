579b08fc722d7c2ee0539cc4d74996f2ad008281
AnkurSheel <ankur.sheel@gmail.com>
2016-02-18 13:12:32 +1300

    - refactored logic for rotation

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -5,52 +5,15 @@ public class Rotation : MonoBehaviour
 {
   public bool isRotateAllowed = false;
   public float speed = 100.0f;
-  Vector3 moveDelta;
 
-  bool isMoving;
-  public bool IsMoving
+  public void Move(Vector3 deltaPosition)
   {
-    set { isMoving = value; }
-  }
-
-  // Use this for initialization
-  void Start()
-  {
-    isMoving = false;
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-//    transform.Rotate(Vector3.up, speed * Time.deltaTime);
-    if (isRotateAllowed && isMoving)
+    if(isRotateAllowed)
     {
-      Vector3 axis = Vector3.zero;
-      if (moveDelta.y > 0.0f)
-      {
-        axis.x = -1.0f;
-      }
-      else if (moveDelta.y < 0.0f)
-      {
-        axis.x = 1.0f;
-      }
-      if (moveDelta.x > 0.0f)
-      {
-        axis.y = -1.0f;
-      }
-      else if (moveDelta.x < 0.0f)
-      {
-        axis.y = 1.0f;
-      }
-      transform.Rotate(moveDelta, speed * Time.deltaTime);
+      Vector3 rot = Vector3.zero;
+      rot.x = -deltaPosition.y * speed;
+      rot.y = -deltaPosition.x * speed;
+      transform.Rotate(rot);
     }
-
-  }
-
-  public void Move(Vector3 position)
-  {
-    isMoving = true;
-    moveDelta = position;
   }
 }
